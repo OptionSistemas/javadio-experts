3d4bd4674f764f6a0f6a0a7e404ef8baddfbec0d
OptionSistemas <optionsistemas@yahoo.com.br>
2021-07-27 11:56:26 -0300

    Add Projects Service Discovery and Gateway

--- gateway/src/main/java/one/digitalinnovation/experts/gateway/GatewayApplication.java
+++ gateway/src/main/java/one/digitalinnovation/experts/gateway/GatewayApplication.java
@@ -1,12 +1,13 @@
-package one.digitalinnovation.experts.productcatalog;
+package one.digitalinnovation.experts.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductCatalogApplication {
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductCatalogApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
+
 }
